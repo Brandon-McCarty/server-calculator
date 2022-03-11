87416b6c1d52c7b5132d5179530010a4c6c2b478
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-11 16:48:44 -0600

    add route for /history

--- server/server.js
+++ server/server.js
@@ -15,24 +15,6 @@ app.use(express.static('server/public'));
 //body parser config
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/calculation', function (req, res) {
-    console.log('POST /calculation', req.body);
-    // run calculation of request
-    calculator(req.body);
-    //set result key equal to the result of the calculation
-    req.body.result = result;
-    // push all needed equation information into history array
-    history.push(req.body);
-    //send response status
-    res.sendStatus(201);
-})
-
-app.get('/calculation', function (req, res) {
-    console.log('GET /calculation');
-    // send result of latest calculation
-    res.send(`${result}`);
-})
-
 function calculator(calculation) {
     // run appropriate calculation
     if (calculation.operator == '+') {
@@ -46,9 +28,46 @@ function calculator(calculation) {
     }
 };
 
+app.post('/calculation', function (req, res) {
+    console.log('POST /calculation', req.body);
+    // run calculation of request
+    calculator(req.body);
+    //set result key equal to the result of the calculation
+    req.body.result = result;
+    // push all needed equation information into history array
+    history.push(req.body);
+    //send response status
+    res.sendStatus(201);
+    //display updated object with result
+    console.log(req.body);
+    
+})
+
+app.get('/calculation', function (req, res) {
+    console.log('GET /calculation');
+    // send result of latest calculation
+    res.send(`${result}`);
+})
+
+app.post('/history', function(req, res) {
+    console.log('POST /history');
+    if (req.body.showHistory === 'yes') {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(401)
+    }
+    
+});
+
+app.get('/history', function(req, res) {
+    console.log('GET /history');
+    //send history to client
+    res.send(history)    
+});
+
 
 
 //start server
 app.listen(PORT, function () {
     console.log('Server running on port', PORT);
-})
+});
