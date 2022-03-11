c029442709c75cf8bfcd6a63aaa7000cb3c0dd5e
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-11 15:39:57 -0600

    add scripts, html, server, and style

---
+++ server/public/scripts/client.js
@@ -0,0 +1,8 @@
+console.log('Client Loaded');
+
+$(handleReady)
+
+function handleReady() {
+    console.log('JQuery Loaded');
+    
+}
---
+++ server/server.js
@@ -0,0 +1,7 @@
+// require express and bodyParser
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//set up server
+const app = express();
+const PORT = 5000;
