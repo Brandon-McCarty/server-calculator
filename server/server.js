6327f5e0619e5da8952f160e733c3610f4da7260
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-11 15:43:14 -0600

    add server functionality

--- server/server.js
+++ server/server.js
@@ -5,3 +5,22 @@ const bodyParser = require('body-parser');
 //set up server
 const app = express();
 const PORT = 5000;
+
+//server static files
+app.use(express.static('server/public'));
+
+//body parser config
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+
+
+
+//start server
+app.listen(PORT, function() {
+    console.log('Server running on port', PORT);    
+})
